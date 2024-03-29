8ff8019c6e1f030fe13f7af06d3a12a4a435f6c0
A Samod <asamod999@gmail.com>
2024-03-29 22:22:38 +0530

    add price filter for explore view

--- ShoppingApp/view/SearchView.swift
+++ ShoppingApp/view/SearchView.swift
@@ -1,3 +1,175 @@
+//import SwiftUI
+////
+////struct SearchView: View {
+////    @StateObject var productsViewModel = ProductsViewModel()
+////    
+////    @State private var gridColumns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
+////    @State private var searchText = ""
+////    
+////    @State private var isAscendingSort = true
+////    
+////    init(searchTerm: String) {
+////        self._searchText = State(initialValue: searchTerm)
+////    }
+////    
+////    var filteredProducts: [Product] {
+////        var filtered = searchText.isEmpty ? productsViewModel.products : productsViewModel.products.filter {
+////            $0.name.lowercased().contains(searchText.lowercased())
+////        }
+////        
+////        if isAscendingSort {
+////            filtered.sort { $0.price < $1.price }
+////        } else {
+////            filtered.sort { $0.price > $1.price }
+////        }
+////        
+////        return filtered
+////    }
+////    
+////    var body: some View {
+////        NavigationView {
+////            VStack {
+////                Picker("Sort By", selection: $isAscendingSort) {
+////                    Text("Low to High").tag(true)
+////                    Text("High to Low").tag(false)
+////                }
+////                .pickerStyle(SegmentedPickerStyle())
+////                .padding()
+////                
+////                ScrollView {
+////                    LazyVGrid(columns: gridColumns, spacing: 25) {
+////                        ForEach(filteredProducts, id: \.id) { product in
+////                            ProductTileView(product: product)
+////                                .padding([.horizontal,.top])
+////                        }
+////                    }
+////                    .background(Color.white.edgesIgnoringSafeArea(.all))
+////                }
+////                .navigationTitle("Discover Your Style")
+////                .searchable(text: $searchText, prompt: "Search Items")
+////            }
+////        }
+////    }
+////}
+//
+//struct SearchView: View {
+//    @StateObject var productsViewModel = ProductsViewModel()
+//    
+//    @State private var gridColumns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
+//    @State private var searchText = ""
+//    
+//    @State private var isAscendingSort = true
+//    
+//    init(searchTerm: String) {
+//        self._searchText = State(initialValue: searchTerm)
+//    }
+//    
+//    var filteredProducts: [Product] {
+//        var filtered = searchText.isEmpty ? productsViewModel.products : productsViewModel.products.filter {
+//            $0.name.lowercased().contains(searchText.lowercased())
+//        }
+//        
+//        if isAscendingSort {
+//            filtered.sort { $0.price < $1.price }
+//        } else {
+//            filtered.sort { $0.price > $1.price }
+//        }
+//        
+//        return filtered
+//    }
+//    
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                HStack {
+//                    Text("Sort By:")
+//                        .font(.headline)
+//                        .foregroundColor(.black)
+//                    Picker("", selection: $isAscendingSort) {
+//                        Text("Low to High").tag(true)
+//                        Text("High to Low").tag(false)
+//                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+//                }
+//                .padding()
+//                
+//                ScrollView {
+//                    LazyVGrid(columns: gridColumns, spacing: 25) {
+//                        ForEach(filteredProducts, id: \.id) { product in
+//                            ProductTileView(product: product)
+//                                .padding([.horizontal,.top])
+//                        }
+//                    }
+//                    .background(Color.white.edgesIgnoringSafeArea(.all))
+//                }
+//                .navigationTitle("Discover Your Style")
+//                .searchable(text: $searchText, prompt: "Search Items")
+//            }
+//        }
+//    }
+//}
+
+//struct SearchView: View {
+//    @StateObject var productsViewModel = ProductsViewModel()
+//    
+//    @State private var gridColumns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
+//    @State private var searchText = ""
+//    
+//    @State private var sortOptions = ["Low to High", "High to Low"]
+//    @State private var selectedSortOption = 0
+//    
+//    init(searchTerm: String) {
+//        self._searchText = State(initialValue: searchTerm)
+//    }
+//    
+//    var filteredProducts: [Product] {
+//        var filtered = searchText.isEmpty ? productsViewModel.products : productsViewModel.products.filter {
+//            $0.name.lowercased().contains(searchText.lowercased())
+//        }
+//        
+//        if selectedSortOption == 0 {
+//            filtered.sort { $0.price < $1.price }
+//        } else {
+//            filtered.sort { $0.price > $1.price }
+//        }
+//        
+//        return filtered
+//    }
+//    
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                HStack {
+//                    Text("Sort By:")
+//                        .font(.headline)
+//                        .foregroundColor(.black)
+//                    Picker("", selection: $selectedSortOption) {
+//                        ForEach(0..<sortOptions.count) { index in
+//                            Text(sortOptions[index])
+//                                .tag(index)
+//                        }
+//                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+//                    .frame(width: 200)
+//                }
+//                .padding()
+//                
+//                ScrollView {
+//                    LazyVGrid(columns: gridColumns, spacing: 25) {
+//                        ForEach(filteredProducts, id: \.id) { product in
+//                            ProductTileView(product: product)
+//                                .padding([.horizontal,.top])
+//                        }
+//                    }
+//                    .background(Color.white.edgesIgnoringSafeArea(.all))
+//                }
+//                .navigationTitle("Discover Your Style")
+//                .searchable(text: $searchText, prompt: "Search Items")
+//            }
+//        }
+//    }
+//}
+
 import SwiftUI
 
 struct SearchView: View {
@@ -7,6 +179,7 @@ struct SearchView: View {
     @State private var searchText = ""
     
     @State private var isAscendingSort = true
+    @State private var selectedPriceFilter = "All"
     
     init(searchTerm: String) {
         self._searchText = State(initialValue: searchTerm)
@@ -23,19 +196,56 @@ struct SearchView: View {
             filtered.sort { $0.price > $1.price }
         }
         
+        if selectedPriceFilter != "All" {
+            let priceRange = getPriceRange(for: selectedPriceFilter)
+            filtered = filtered.filter { product in
+                if let price = Double(product.price) {
+                    return priceRange.contains(price)
+                } else {
+                    return false
+                }
+            }
+        }
+        
         return filtered
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                Picker("Sort By", selection: $isAscendingSort) {
-                    Text("Low to High").tag(true)
-                    Text("High to Low").tag(false)
+                Divider().padding(.horizontal)
+                HStack {
+                    Text("Sort By")
+                        .font(.headline)
+                        .padding(.leading)
+                    
+                 //   Spacer()
+                    
+                    Toggle(isOn: $isAscendingSort) {
+                        Text("Low to High")
+                    }
+                    .padding(.trailing)
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-                
+                .padding(.vertical)
+                Divider().padding(.horizontal)
+                HStack {
+                    Text("Filter By Price")
+                        .font(.headline)
+                        .padding(.leading)
+                    
+                    Spacer()
+                    
+                    Menu(selectedPriceFilter) {
+                        ForEach(["All", "Under LKR 500", "LKR 500 - LKR 1500", "LKR 1500 - LKR 2500", "Over LKR 2500"], id: \.self) { filter in
+                            Button(filter) {
+                                selectedPriceFilter = filter
+                            }
+                        }
+                    }
+                    .padding(.trailing)
+                }
+                .padding(.vertical)
+                Divider().padding(.horizontal)
                 ScrollView {
                     LazyVGrid(columns: gridColumns, spacing: 25) {
                         ForEach(filteredProducts, id: \.id) { product in
@@ -45,9 +255,26 @@ struct SearchView: View {
                     }
                     .background(Color.white.edgesIgnoringSafeArea(.all))
                 }
-                .navigationTitle("Discover Your Style")
-                .searchable(text: $searchText, prompt: "Search Items")
+//                .navigationTitle("Discover Your Style")
+//                Spacer()
+                .searchable(text: $searchText, prompt: "Discover Your Style")
             }
         }
     }
+    
+    func getPriceRange(for filter: String) -> ClosedRange<Double> {
+        switch filter {
+        case "Under LKR 500":
+            return 0.0...499.99
+        case "LKR 500 - LKR 1500":
+            return 500.0...1499.99
+        case "LKR 1500 - LKR 2500":
+            return 1500.0...2499.00
+        case "Over LKR 2500":
+            return 2500.0...Double.greatestFiniteMagnitude
+        default:
+            return 0.0...Double.greatestFiniteMagnitude
+        }
+    }
 }
+
